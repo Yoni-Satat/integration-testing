f249c22057d3139d51d9215975625d5086880b93
Yoni Satat <yoni.satat@gmail.com>
2018-02-15 01:30:20 +0000

    first test passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -11,11 +11,19 @@ describe('calculator functionality', function() {
 
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
+    running_total = element(by.css('#running_total'));
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   });
 
-  // Do the number buttons work to update the display of the running total?
+  it('should update the display of the running total', function(){
+     running_total = element(by.css('#running_total'))
+     element(by.css('#number6')).click();
+     element(by.css('#operator_subtract')).click();
+     element(by.css('#number4')).click();
+     element(by.css('#operator_equals')).click();
+     expect(running_total.getAttribute('value')).to.eventually.equal('2');
+   });
 
+   
 });
