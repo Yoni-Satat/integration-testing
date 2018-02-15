5aa8d82167535b301959b56b52e4316bbaf9cb23
Yoni Satat <yoni.satat@gmail.com>
2018-02-15 01:34:54 +0000

    third test passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -34,5 +34,14 @@ describe('calculator functionality', function() {
       expect(running_total.getAttribute('value')).to.eventually.equal('10');
     });
 
+    it('should have working multiply button', function(){
+       running_total = element(by.css('#running_total'))
+       element(by.css('#number6')).click();
+       element(by.css('#operator_multiply')).click();
+       element(by.css('#number4')).click();
+       element(by.css('#operator_equals')).click();
+       expect(running_total.getAttribute('value')).to.eventually.equal('24');
+     });
+
 
 });
