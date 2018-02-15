ac9e8e5f3e940c6f283c4fd6e21bead7434616f5
Yoni Satat <yoni.satat@gmail.com>
2018-02-15 01:40:59 +0000

    fifth test passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -52,5 +52,21 @@ describe('calculator functionality', function() {
         expect(running_total.getAttribute('value')).to.eventually.equal('2');
       });
 
+      it('should have multiple operators chained together', function(){
+         running_total = element(by.css('#running_total'))
+         element(by.css('#number6')).click();
+         element(by.css('#operator_divide')).click();
+         element(by.css('#number3')).click();
+         element(by.css('#operator_add')).click();
+         element(by.css('#number3')).click();
+         element(by.css('#operator_multiply')).click();
+         element(by.css('#number3')).click();
+         element(by.css('#operator_subtract')).click();
+         element(by.css('#number1')).click();
+         element(by.css('#number5')).click();
+         element(by.css('#operator_equals')).click();
+         expect(running_total.getAttribute('value')).to.eventually.equal('0');
+       });
+
 
 });
